7ea9e1505e22a037fb033a9ceaa69e44dd85c7bc
Eliasnew52 <xxeliasnew52xx@gmail.com>
2023-08-20 19:45:53 -0600

    Added Guna Nugget , Some Window Config Done

--- GreenGuardianMK2/Form1.Designer.cs
+++ GreenGuardianMK2/Form1.Designer.cs
@@ -29,17 +29,30 @@
         private void InitializeComponent()
         {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.NavBar = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // NavBar
+            // 
+            this.NavBar.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.NavBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.NavBar.Location = new System.Drawing.Point(0, 0);
+            this.NavBar.Name = "NavBar";
+            this.NavBar.Size = new System.Drawing.Size(1924, 100);
+            this.NavBar.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 647);
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.NavBar);
             this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IsMdiContainer = true;
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Green Guardian MK2 ";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
         }
@@ -47,6 +60,7 @@
         #endregion
 
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel NavBar;
     }
 }
 
